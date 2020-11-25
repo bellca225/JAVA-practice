188aef4e692f52791124bdba822784b15c6f1448
ny <nywoo0225@gmail.com>
2020-11-25 11:22:59 +0900

    파일 정리

--- src/ExceptionTest.java
+++ src/ExceptionTest.java
@@ -5,13 +5,17 @@ public class ExceptionTest {
         try {
             for (int i = 0; i <= 5; i++) {
                 System.out.println(nums[i]);
-                //            범위를 넘어서는 예외처리
+                // 범위를 넘어서는 예외 처리.
+                // 예외처리를 만난 후 for 구문 안은 잠시 멈추지만, 프로그램은 지속적으로 진행된다.
             }
+
         }catch (Exception e){
             System.out.println("범위를 넘어서는 예외가 발생했습니다.");
             System.out.println(e.getMessage()); // 메세지 가져오기
-          //e.printStackTrace();
-        } //catch 문을 추가로 더 쓸 수도 있음.
+          //e.printStackTrace(); //catch 문을 추가로 더 쓸 수도 있음.
+        }finally {
+            System.out.println("꼭 처리해야줘야 할 일");
+        }
 
 
         System.out.println("예외가 발생해도 프로그램은 실행 중입니다.");
